d4dc295657a9392749688a3f4cf79f48fef4dc85
Anamul Haque <anamulah535367@gmail.com>
2023-07-09 15:18:43 +0600

    dbCon .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME, // your username
+      password: process.env.DB_PASSWORD, // your password
+      database: process.env.DB_DATABASE, // name of database
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
